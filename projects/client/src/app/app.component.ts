4c1ebb9f013a1a93134c7fe0894f43689e101256
ryzhovalex <thed4rkof@gmail.com>
2024-03-18 13:34:39 +0300

    add ngx-kit

--- projects/client/src/app/app.component.ts
+++ projects/client/src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { AlertService } from "@almazrpe/ngx-kit";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent
+export class AppComponent implements OnInit
 {
   public title = "client";
+
+  public constructor(
+    private alertSv: AlertService
+  )
+  {
+  }
+
+  public ngOnInit()
+  {
+
+  }
 }
