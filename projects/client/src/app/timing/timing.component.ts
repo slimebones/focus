999378c3c194031cdf1ff8c7baedd27247c93b0c
ryzhovalex <thed4rkof@gmail.com>
2024-05-29 14:57:25 +0300

    update pause.png, play.png, timing.component.html, ...

--- projects/client/src/app/timing/timing.component.ts
+++ projects/client/src/app/timing/timing.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
-import { BehaviorSubject, Subscription } from "rxjs";
+import {
+  BehaviorSubject, Observable, Subscription, map, of, switchMap } from "rxjs";
 import { TimingService } from "./timing.service";
+import { TimerUdto } from "./models";
 
 @Component({
   selector: "app-timing",
@@ -10,11 +12,18 @@ import { TimingService } from "./timing.service";
 })
 export class TimingComponent implements OnInit, OnDestroy
 {
+  public currentTimer$: Observable<TimerUdto>;
+
+  private readonly PLAY_BTN_IMG_URL = "assets/play.png";
+  private readonly PAUSE_BTN_IMG_URL = "assets/pause.png";
+  public togglePlayBtnImgUrl: string;
+
   public isEnabled: boolean = true;
   public toggleBtnSelectors$: BehaviorSubject<string[]> =
     new BehaviorSubject(["filter-white"]);
 
   private subs: Subscription[] = [];
+  private readonly DEFAULT_TIMER_DURATION = 60 * 30;
 
   public readonly BASIC_GROUP_SELECTOR_CLASSES: string[] = [
     "border", "rounded"
@@ -36,6 +45,27 @@ export class TimingComponent implements OnInit, OnDestroy
 
   public ngOnInit(): void
   {
+    this.currentTimer$ = this.timingSv.getTimers$().pipe(
+      switchMap(timers =>
+      {
+        if (timers.length == 0)
+        {
+          return this.timingSv.createTimer$(this.DEFAULT_TIMER_DURATION);
+        }
+        return of(timers[0]);
+      }),
+      map(timer =>
+      {
+        switch (timer.status)
+        {
+          case "tick":
+            this.togglePlayBtnImgUrl = this.PAUSE_BTN_IMG_URL;
+            break;
+          // for paused and finished states
+          default:
+            this.togglePlayBtnImgUrl = this.PLAY_BTN_IMG_URL;
+        }
+      }));
   }
 
   public ngOnDestroy(): void
@@ -52,5 +82,9 @@ export class TimingComponent implements OnInit, OnDestroy
     this.isEnabled
       ? this.toggleBtnSelectors$.next(["filter-gray"])
       : this.toggleBtnSelectors$.next(["filter-white"]);
+  }
+
+  public togglePlay()
+  {
   }
 }
--- projects/client/src/app/timing/timing.service.ts
+++ projects/client/src/app/timing/timing.service.ts
@@ -91,37 +91,13 @@ export class TimingService
     }));
   }
 
-  public createGroup$(name: string): Observable<TimerGroupUdto>
-  {
-    return BusUtils.pubCreateDocReq$(new CreateDocReq({
-      collection: this.GROUP_COLLECTION,
-      createQuery: {
-        name: name
-      }
-    }));
-  }
-
-  public createTimerForGroup$(
-    duration: number, groupSid: string): Observable<TimerUdto>
+  public createTimer$(duration: number): Observable<TimerUdto>
   {
     return BusUtils.pubCreateDocReq$<TimerUdto>(new CreateDocReq({
       collection: this.TIMER_COLLECTION,
       createQuery: {
         total_duration: duration
       }
-    })).pipe(switchMap(timer_udto =>
-    {
-      return BusUtils.pubUpdDocReq$(new UpdDocReq({
-        collection: this.GROUP_COLLECTION,
-        searchQuery: {
-          sid: groupSid
-        },
-        updQuery: {
-          "$push": {
-            "timer_sids": timer_udto.sid
-          }
-        }
-      })).pipe(map(_ => {return timer_udto;}));
     }));
   }
 
