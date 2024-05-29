8aee2902e8cb03505a5cab7fc5cd44566b6d865b
ryzhovalex <thed4rkof@gmail.com>
2024-05-29 14:26:55 +0300

    update undo_arrow_white.png, undo_arrow_white.png~, ...

--- projects/client/tailwind.config.js
+++ projects/client/tailwind.config.js
@@ -8,23 +8,45 @@ module.exports = {
   ],
   theme: {
     extend: {
+      // windows xp palette https://lospec.com/palette-list/windows-xp-color
       // fg => foreground
       // bg => background
       colors: {
-        "c60-bg": "#070F2B",
+        "c60-bg": "#000000",
         "c60-fg": "#FFFFFF",
 
-        "c30-bg": "#1B1A55",
-        "c30-fg": "#FFFFFF",
+        "c30-bg": "#FFFFFF",
+        "c30-fg": "#000000",
 
+        "c10-bg": "#75b600",
         "c10-fg": "#FFFFFF",
-        "c10-bg": "#535C91",
 
-        "c10-bg-active": "#9290C3",
+        "c10-bg-active": "#a2fa07",
         "c10-fg-active": "#FFFFFF",
 
         "c-fg-disabled": "#FFFFFF",
-        "c-bg-disabled": colors.gray["300"]
+        "c-bg-disabled": colors.gray["300"],
+
+        "header1-bg": "#a1b5fe",
+        "header1-fg": "#ffffff",
+        "header2-bg": "#485fea",
+        "header2-fg": "#ffffff",
+        "header3-bg": "#1621c9",
+        "header3-fg": "#ffffff",
+
+        "danger1-bg": "#fea75d",
+        "danger1-fg": "#ffffff",
+        "danger2-bg": "#e55000",
+        "danger2-fg": "#ffffff",
+        "danger3-bg": "#9b1e00",
+        "danger3-fg": "#ffffff",
+
+        "warn1-bg": "#def222",
+        "warn1-fg": "#ffffff",
+        "warn2-bg": "#d3a801",
+        "warn2-fg": "#ffffff",
+        "warn3-bg": "#8a7500",
+        "warn3-fg": "#ffffff",
       },
       animation: {
         "spin-slow": "spin 3s linear infinite",
