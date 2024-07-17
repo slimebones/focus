4e1b1e15cad690d8e13a23964906739cc2aaca14
ryzhovalex <thed4rkof@gmail.com>
2024-07-17 23:21:47 +0300

    update res.h, uuid.h, .gitmodules, sessions.json, ...

--- projects/desktop/desktop/rxcat/rxcat.h
+++ projects/desktop/desktop/rxcat/rxcat.h
@@ -1,8 +1,40 @@
 #pragma once
+#include <functional>
+#include "../ckit/uuid.h"
+
 namespace Rxcat
 {
+	class Msg
+	{
+	public:
+		std::string msid = Ckit::make_uuid4();
+	};
+
+	class Evt: public Msg
+	{
+	public:
+		std::string rsid;
+		bool* is_thrown_by_pubfn = nullptr;
+	};
+
+	class Req: public Msg
+	{
+	};
+
 	class ClientBus
 	{
+	private:
+		template<class TReq, class TEvt>
+		using Pubfn = std::function<void(TReq, TEvt)>;
+		template<class TMsg>
+		using Subfn = std::function<void(TMsg)>;
+
+		struct ReqAndPubfn
+		{
+		public:
+			Req req;
+			Pubfn<Req, Evt> pubfn;
+		};
 	};
 }
 
